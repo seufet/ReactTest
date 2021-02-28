296e70bca3920afcd9380d78d0823e7bbfd73824
seufet <tom.seufert@gmail.com>
2021-02-27 22:23:56 -0500

    can show errors now!

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from './shared/Header';
+import HeaderContainer from './shared/HeaderContainer';
+import ErrorBox from './shared/ErrorBoxContainer';
 import HomePage from './home/HomePageContainer';
 import ProfilePage from './account/ProfilePage';
 import LoginPage from './account/LoginPageContainer';
+import RegisterPage from './account/RegisterPageContainer';
+import RegistrationSuccessPage from './account/RegistrationSuccessPageContainer';
 
 export default function Template(props) {
+  const { authentication, progress } = props;
   return (
     <Router>
       <div className="wrapper">
-        <Header username="anonymous" />
-        <p>{props.progress}</p>
+        <HeaderContainer authentication={authentication} />
+        <ErrorBox />
         <section className="page-content container-fluid">
-          <Route exact path="/" component={HomePage} />
-          <Route path="/account/profile/:id" component={ProfilePage} />
+          <Route exact path="/" component={HomePage} />          
           <Route exact path="/account/login" component={LoginPage} />
+          <Route path="/account/profile/:id" component={ProfilePage} />
+          <Route exact path="/account/register" component={RegisterPage} />
+          <Route exact path="/account/registration-success" component={RegistrationSuccessPage} />
         </section>
+
+        <div className="loader-wrapper" style={progress > 0 ? { display: 'block' } : { display: 'none' }}>
+          <div className="loader-box">
+            <div className="loader">Loading...</div>
+          </div>
+        </div>
       </div>
     </Router>
   );
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -2,14 +2,33 @@ import React from 'react';
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+const renderLogin = () => (
+  <Nav className="ml-auto" navbar>
+    <NavItem>
+      <NavLink tag={Link} to="/account/login">Log In</NavLink>
+    </NavItem>
+    <NavItem>
+      <NavLink tag={Link} to="/account/register">Register</NavLink>
+    </NavItem>
+  </Nav>
+);
+
 export default class Header extends React.Component {
   constructor(props) {
     super(props);
 
+    this.logOutClick = this.logOutClick.bind(this);
+    this.renderGreeting = this.renderGreeting.bind(this);
     this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
       isOpen: false,
     };
+  }
+
+  logOutClick(e) {
+    e.preventDefault();
+    const { logUserOutFunction } = this.props;
+    logUserOutFunction();
   }
 
   toggleNavbar() {
@@ -18,18 +37,25 @@ export default class Header extends React.Component {
     });
   }
 
+  renderGreeting(name) {
+    return (
+      <Nav className="ml-auto" navbar>
+        <NavItem>
+          Welcome, {name} | <a href="/logout" onClick={this.logOutClick}>Log Out</a>
+        </NavItem>
+      </Nav>
+    );
+  }
+
   render() {
+    const { isLoggedIn, firstName } = this.props.authentication;
     return (
       <header className="wrapper">
         <Navbar color="faded" light toggleable>
           <NavbarToggler right onClick={this.toggleNavbar} />
           <NavbarBrand tag={Link} to="/">ReactTest</NavbarBrand>
           <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink tag={Link} to="/account/login">Log In</NavLink>
-              </NavItem>
-            </Nav>
+            { isLoggedIn ? this.renderGreeting(firstName) : renderLogin() }
           </Collapse>
         </Navbar>
       </header>
